f1ad9ddd6926a82c825771467e4ecaa2a4e97a2a
Chris Kent <cjkent@hotmail.com>
2017-06-11 23:41:46 +0100

    Tidying

--- api/src/main/kotlin/io/github/cjkent/osiris/api/Api.kt
+++ api/src/main/kotlin/io/github/cjkent/osiris/api/Api.kt
@@ -310,6 +310,8 @@ class ApiBuilder<T : ApiComponents> private constructor(
         filters.add(Filter(prefix + path, handler))
     }
 
+    fun filter(handler: FilterHandler<T>): Unit = filter("/*", handler)
+
     // TODO not sure about this any more because of its interaction with filters.
     // a path can define a variable segment which doesn't make a lot of sense for a filter.
     // or should a filter treat a variable section like a wildcard? what does SparkJava do?
--- api/src/test/kotlin/io/github/cjkent/osiris/api/FilterTest.kt
+++ api/src/test/kotlin/io/github/cjkent/osiris/api/FilterTest.kt
@@ -29,15 +29,13 @@ class FilterTest {
 
         val (matchHandler1, _) = node.match(HttpMethod.GET, "/")!!
         val req1 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val handlerVal1 = matchHandler1(components, req1)
-        val response1 = handlerVal1 as Response
+        val response1 = matchHandler1(components, req1) as Response
         assertEquals("ROOT", response1.body)
         assertEquals(ContentTypes.APPLICATION_XML, response1.headers[HttpHeaders.CONTENT_TYPE])
 
         val (matchHandler2, _) = node.match(HttpMethod.GET, "/foo")!!
         val req2 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val handlerVal2 = matchHandler2(components, req2)
-        val response2 = handlerVal2 as Response
+        val response2 = matchHandler2(components, req2) as Response
         assertEquals("FOO", response2.body)
         assertEquals(ContentTypes.APPLICATION_XML, response2.headers[HttpHeaders.CONTENT_TYPE])
     }
@@ -62,8 +60,7 @@ class FilterTest {
             val newReq = req.copy(
                 defaultResponseHeaders = mapOf("foo" to fooHeader + "2")
             )
-            val handlerVal = handler(this, newReq)
-            val response = handlerVal as Response
+            val response = handler(this, newReq) as Response
             response.copy(body = response.body.toString() + "2")
         }
         val filter2 = Filter("/*", filterHandler2)
@@ -74,8 +71,7 @@ class FilterTest {
 
         val (matchHandler1, _) = node.match(HttpMethod.GET, "/")!!
         val req1 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val handlerVal1 = matchHandler1(components, req1)
-        val response1 = handlerVal1 as Response
+        val response1 = matchHandler1(components, req1) as Response
         assertEquals("12", response1.headers["foo"])
         assertEquals("root21", response1.body)
 
@@ -89,12 +85,11 @@ class FilterTest {
 
     fun filterInApi() {
         val api = api(ApiComponents::class) {
-            filter("") { req, handler ->
+            filter { req, handler ->
                 val newReq = req.copy(
                     defaultResponseHeaders = mapOf(HttpHeaders.CONTENT_TYPE to ContentTypes.APPLICATION_XML)
                 )
-                val handlerVal = handler(this, newReq)
-                val response = handlerVal as Response
+                val response = handler(this, newReq) as Response
                 response.copy(body = (response.body as String).toUpperCase())
 
             }
