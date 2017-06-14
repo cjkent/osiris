246e122d95edac096f918224dd231a6aeb0b2d11
Chris Kent <cjkent@hotmail.com>
2017-06-14 22:31:36 +0100

    Fix filter tests

--- api/src/test/kotlin/io/github/cjkent/osiris/api/FilterTest.kt
+++ api/src/test/kotlin/io/github/cjkent/osiris/api/FilterTest.kt
@@ -9,10 +9,6 @@ import kotlin.test.assertTrue
 class FilterTest {
 
     fun simpleFilter() {
-        val handler1: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("root") }
-        val route1 = Route(HttpMethod.GET, "/", handler1)
-        val handler2: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("foo") }
-        val route2 = Route(HttpMethod.GET, "/foo", handler2)
         // Filter that changes the response content type to XML by modifying the request and converts the response
         // body to upper case by modifying the response
         val filterHandler: FilterHandler<ApiComponents> = { req, handler ->
@@ -23,32 +19,24 @@ class FilterTest {
             response.copy(body = (response.body as String).toUpperCase())
         }
         val filter = Filter("/*", filterHandler)
-        // TODO the filters aren't applied unless the routes are built using ApiBuilder
-        // Would it be cleaner to move the logic to Route? Route.wrap(filters)?
-        val api = Api(listOf(route1, route2), listOf(filter), ApiComponents::class)
-        val node = RouteNode.create(api)
+        val handler1: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("root") }
+        val handler2: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("foo") }
+        val route1 = Route(HttpMethod.GET, "/", handler1).wrap(listOf(filter))
+        val route2 = Route(HttpMethod.GET, "/foo", handler2).wrap(listOf(filter))
         val components = object : ApiComponents {}
 
-        // TODO don't use a match here, test the route directly
-        val (matchHandler1, _) = node.match(HttpMethod.GET, "/")!!
         val req1 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val response1 = matchHandler1(components, req1)
+        val response1 = route1.handler(components, req1)
         assertEquals("ROOT", response1.body)
         assertEquals(ContentTypes.APPLICATION_XML, response1.headers[HttpHeaders.CONTENT_TYPE])
 
-        // TODO don't use a match here, test the route directly
-        val (matchHandler2, _) = node.match(HttpMethod.GET, "/foo")!!
         val req2 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val response2 = matchHandler2(components, req2)
+        val response2 = route2.handler(components, req2)
         assertEquals("FOO", response2.body)
         assertEquals(ContentTypes.APPLICATION_XML, response2.headers[HttpHeaders.CONTENT_TYPE])
     }
 
     fun multipleFilters() {
-        val handler1: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("root") }
-        val route1 = Route(HttpMethod.GET, "/", handler1)
-        val handler2: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("foo") }
-        val route2 = Route(HttpMethod.GET, "/foo", handler2)
         val filterHandler1: FilterHandler<ApiComponents> = { req, handler ->
             val newReq = req.copy(
                 defaultResponseHeaders = mapOf("foo" to "1")
@@ -56,8 +44,6 @@ class FilterTest {
             val response = handler(this, newReq)
             response.copy(body = response.body.toString() + "1")
         }
-        val filter1 = Filter("/*", filterHandler1)
-
         val filterHandler2: FilterHandler<ApiComponents> = { req, handler ->
             val fooHeader = req.defaultResponseHeaders["foo"]!!
             val newReq = req.copy(
@@ -66,21 +52,23 @@ class FilterTest {
             val response = handler(this, newReq)
             response.copy(body = response.body.toString() + "2")
         }
+        val filter1 = Filter("/*", filterHandler1)
         val filter2 = Filter("/*", filterHandler2)
+        val filters = listOf(filter1, filter2)
+        val handler1: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("root") }
+        val handler2: RequestHandler<ApiComponents> = { req -> req.responseBuilder().build("foo") }
+        val route1 = Route(HttpMethod.GET, "/", handler1).wrap(filters)
+        val route2 = Route(HttpMethod.GET, "/foo", handler2).wrap(filters)
 
-        val api = Api(listOf(route1, route2), listOf(filter1, filter2), ApiComponents::class)
-        val node = RouteNode.create(api)
         val components = object : ApiComponents {}
 
-        val (matchHandler1, _) = node.match(HttpMethod.GET, "/")!!
         val req1 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val response1 = matchHandler1(components, req1)
+        val response1 = route1.handler(components, req1)
         assertEquals("12", response1.headers["foo"])
         assertEquals("root21", response1.body)
 
-        val (matchHandler2, _) = node.match(HttpMethod.GET, "/foo")!!
         val req2 = Request(HttpMethod.GET, "/", Params(), Params(), Params(), null, false)
-        val response2 = matchHandler2(components, req2)
+        val response2 = route2.handler(components, req2)
         assertEquals("12", response1.headers["foo"])
         assertEquals("foo21", response2.body)
     }
