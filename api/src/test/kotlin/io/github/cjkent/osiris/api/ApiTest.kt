ea1dd24d8375cd33551ca816bbb8427b82bb4a54
Chris Kent <cjkent@hotmail.com>
2017-06-15 20:42:07 +0100

    Basic test for creating an API with the DSL

--- api/src/test/kotlin/io/github/cjkent/osiris/api/ApiTest.kt
+++ api/src/test/kotlin/io/github/cjkent/osiris/api/ApiTest.kt
@@ -1,6 +1,7 @@
 package io.github.cjkent.osiris.api
 
 import org.testng.annotations.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 @Test
@@ -23,5 +24,29 @@ class ApiTest {
         assertFailsWith<IllegalArgumentException> { Route.validatePath("/{fo}o") }
     }
 
-    // TODO test that builds a trivial API including a components interface and impl. to map sure the type sigs work
+    /**
+     * Tests creating a very simple API using the DSL, a components interface and components implementation.
+     *
+     * This is mostly to make sure I don't break the type signatures if I'm mucking around with the generics.
+     */
+    fun createApi() {
+        val api = api(TestComponentsImpl::class) {
+            get("/foo") {
+                foo
+            }
+            get("/bar") {
+                bar
+            }
+        }
+        assertEquals(2, api.routes.size)
+        assertEquals("/foo", api.routes[0].path)
+        assertEquals("/bar", api.routes[1].path)
+    }
+
+    interface TestComponents : ApiComponents {
+        val foo: String
+        val bar: Int
+    }
+
+    class TestComponentsImpl(override val foo: String, override val bar: Int) : TestComponents
 }
