7852b0044a16603804d99a6b61efc06a7a275ee7
Chris Kent <cjkent@hotmail.com>
2017-06-16 21:00:00 +0100

    Tests for functionality that will be provided by standard filters

--- api/src/test/kotlin/io/github/cjkent/osiris/api/StandardFilterTest.kt
+++ api/src/test/kotlin/io/github/cjkent/osiris/api/StandardFilterTest.kt
@@ -1,7 +1,10 @@
 package io.github.cjkent.osiris.api
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.testng.annotations.Test
+import java.util.regex.Pattern
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * Tests for the functionality provided by the standard set of filters that are included by default.
@@ -32,7 +35,7 @@ class StandardFilterTest {
      *     * null - no body
      *     * string - used as-is, no base64 - Jackson should handle escaping when AWS does the conversion
      *     * ByteArray - base64 encoded - does API Gateway return this as binary? or a base64 encoded string?
-     *     * any other type throws an exception
+     *     * any other type throws an exception. or should it just use toString()? seems friendlier
      */
     fun serialiseObjectsToJson() {
         val client = InMemoryTestClient.create {
@@ -55,8 +58,46 @@ class StandardFilterTest {
         assertEquals("""{"foo":42,"bar":"Bar"}""", client.get("/objectbody").body)
     }
 
-    fun exceptionMapping() {
+    // TODO serialisation when the content type isn't JSON. not a high priority for now
 
+    fun exceptionMapping() {
+        val client = InMemoryTestClient.create {
+            get("/badrequest") { req ->
+                throw IllegalArgumentException("illegal arg")
+            }
+            get("/notfound") { req ->
+                throw DataNotFoundException("not found")
+            }
+            get("/badjson") { req ->
+                // This throws a JsonParseException which is mapped to a bad request
+                jacksonObjectMapper().readValue("this is invalid JSON", Map::class.java)
+            }
+            get("/forbidden") { req ->
+                throw ForbiddenException("top secret")
+            }
+            get("/servererror") { req ->
+                throw RuntimeException("oh no!")
+            }
+        }
+        val (status1, _, body1) = client.get("/badrequest")
+        assertEquals(400, status1)
+        assertEquals("illegal arg", body1)
+
+        val (status2, _, body2) = client.get("/notfound")
+        assertEquals(404, status2)
+        assertEquals("not found", body2)
+
+        val (status3, _, body3) = client.get("/badjson")
+        assertEquals(400, status3)
+        assertTrue(Pattern.matches("Failed to pars JSON.*", body3 as String))
+
+        val (status4, _, body4) = client.get("/forbidden")
+        assertEquals(403, status4)
+        assertEquals("top secret", body4)
+
+        val (status5, _, body5) = client.get("/servererror")
+        assertEquals(500, status5)
+        assertEquals("Server Error", body5)
     }
 
     //--------------------------------------------------------------------------------------------------
