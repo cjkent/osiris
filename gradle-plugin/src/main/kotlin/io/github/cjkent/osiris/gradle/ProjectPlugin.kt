674de3ba48f4e983265e7bd89fbd132e3cdc98c8
Chris Kent <chris@opengamma.com>
2018-03-06 12:59:51 +0000

    Fix structure of generated Gradle project

--- gradle-plugin/src/main/kotlin/io/github/cjkent/osiris/gradle/ProjectPlugin.kt
+++ gradle-plugin/src/main/kotlin/io/github/cjkent/osiris/gradle/ProjectPlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 /**
@@ -41,11 +42,20 @@ open class OsirisProjectTask : DefaultTask() {
                 .replace("\${osirisVersion}", project.properties["osirisVersion"].toString())
                 .replace("\${kotlinVersion}", project.properties["kotlinVersion"].toString())
             val projectDir = project.projectDir.toPath()
-            val path = projectDir.resolve(resource.substring("/archetype-resources/".length))
+            val path = projectDir.resolve(insertPackageDirs(resource, rootPackage))
             Files.createDirectories(path.parent)
             logger.info("Writing file {} to {}", path.fileName, path.toAbsolutePath())
             Files.write(path, replacedFile.toByteArray(Charsets.UTF_8))
         }
+        Files.createDirectories(project.projectDir.toPath().resolve("core/src/main/static"))
+    }
+
+    private fun insertPackageDirs(resource: String, rootPackage: String): Path {
+        val match = packageRegex.find(resource) ?: return Paths.get(resource.substring("/archetype-resources/".length))
+        val packagePath = rootPackage.replace('.', '/')
+        val groupValues = match.groupValues
+        val path = "${groupValues[1]}/src/main/kotlin/$packagePath/${groupValues[2]}/${groupValues[3]}"
+        return Paths.get(path)
     }
 
     private fun checkPaths(vararg paths: String) {
@@ -72,5 +82,7 @@ open class OsirisProjectTask : DefaultTask() {
             "/archetype-resources/local-server/src/main/resources/log4j2.xml",
             "/archetype-resources/local-server/src/main/kotlin/localserver/Main.kt"
         )
+
+        private val packageRegex: Regex = Regex("/archetype-resources/(.+?)/src/main/kotlin/(.+?)/(.*)")
     }
 }
