85bfde4488b68b5a7ba11284f2991f573cd033d3
Chris Kent <cjkent@hotmail.com>
2018-03-16 22:56:46 +0000

    Simplify the generated API definition
    Fixes #90

--- archetype/src/main/resources/archetype-resources/core/src/main/kotlin/core/ApiDefinition.kt
+++ archetype/src/main/resources/archetype-resources/core/src/main/kotlin/core/ApiDefinition.kt
@@ -1,88 +1,59 @@
 package ${package}.core
 
-import io.github.cjkent.osiris.aws.IamAuth
 import io.github.cjkent.osiris.core.MimeTypes
-import io.github.cjkent.osiris.core.DataNotFoundException
-import io.github.cjkent.osiris.core.ForbiddenException
 import io.github.cjkent.osiris.core.ComponentsProvider
 import io.github.cjkent.osiris.core.HttpHeaders
 import io.github.cjkent.osiris.core.api
 
 /**
- * The name of an environment variable used to pass configuration to the code that handles the HTTP requests.
+ * The name of an environment variable used to pass in configuration.
  *
- * The values of environment variables can be specified in the `environmentVariables` configuration element in the
- * Maven plugin. For example:
+ * The values of environment variables can be specified by the `environmentVariables` property in the configuration:
  *
- *     <environmentVariables>
- *         <EXAMPLE_ENVIRONMENT_VARIABLE>Example value</EXAMPLE_ENVIRONMENT_VARIABLE>
- *     </environmentVariables>
+ *     val config = ApplicationConfig(
+ *         environmentVariables = mapOf(
+ *             "EXAMPLE_ENVIRONMENT_VARIABLE" to "Bob"
+ *         )
+ *         ...
+ *     )
  */
 const val EXAMPLE_ENVIRONMENT_VARIABLE = "EXAMPLE_ENVIRONMENT_VARIABLE"
 
 /** The API. */
 val api = api<ExampleComponents> {
+
     get("/helloworld") { _ ->
         // return a map that is automatically converted to JSON
         mapOf("message" to "hello, world!")
     }
+
     get("/helloplain") { req ->
         // return a response with customised headers
         req.responseBuilder()
             .header(HttpHeaders.CONTENT_TYPE, MimeTypes.TEXT_PLAIN)
             .build("hello, world!")
     }
-    get("/hello/queryparam1") { req ->
-        // get an optional query parameter
-        val name = req.queryParams.optional("name") ?: "world"
-        mapOf("message" to "hello, $name!")
-    }
-    get("/hello/queryparam2") { req ->
-        // get a required query parameter
+
+    get("/helloqueryparam") { req ->
+        // get a query parameter
         val name = req.queryParams["name"]
         mapOf("message" to "hello, $name!")
     }
-    // use path() to group multiple endpoints under the same sub-path
-    path("/hello") {
-        get("/{name}") { req ->
-            // get a path parameter
-            val name = req.pathParams["name"]
-            // this will be automatically converted to a JSON object like {"message":"hello, Bob!"}
-            JsonMessage("hello, $name!")
-        }
-        get("/env") { _ ->
-            // use the name property from ExampleComponents for the name
-            JsonMessage("hello, $name!")
-        }
+
+    get("/helloenv") { _ ->
+        // use the name property from ExampleComponents for the name
+        mapOf("message" to "hello, $name!")
     }
-    // require authorisation for all endpoints inside the auth block
-    auth(IamAuth) {
-        // this will be inaccessible unless a policy is created and attached to the calling user, role or group
-        get("/topsecret") { _ ->
-            JsonMessage("For your eyes only")
-        }
-    }
-    // Endpoints demonstrating the mapping of exceptions to responses
-    // Demonstrates mapping DataNotFoundException to a 404
-    get("/foo/{fooId}") { req ->
-        val fooId = req.pathParams["fooId"]
-        when (fooId) {
-            "123" -> JsonMessage("foo 123 found")
-            else -> throw DataNotFoundException("No foo found with ID $fooId")
-        }
-    }
-    // Status 403 (forbidden)
-    get("/forbidden") { _ ->
-        throw ForbiddenException("top secret")
-    }
-    // Status 500 (server error). Returned when there is not specific handler for the exception type
-    get("/servererror") { _ ->
-        throw RuntimeException("oh no!")
+
+    get("/hello/{name}") { req ->
+        // get a path parameter
+        val name = req.pathParams["name"]
+        mapOf("message" to "hello, $name!")
     }
 }
 
 /**
- * Creates the components used by the test API.
+ * Creates the components used by the example API.
  */
 fun createComponents(): ExampleComponents = ExampleComponentsImpl()
 
@@ -100,12 +71,3 @@ class ExampleComponentsImpl : ExampleComponents {
     override val name: String = System.getenv(EXAMPLE_ENVIRONMENT_VARIABLE) ?:
         "[Environment variable EXAMPLE_ENVIRONMENT_VARIABLE not set]"
 }
-
-/**
- * Simple class demonstrating automatic conversion to JSON.
- *
- * Produces JSON like:
- *
- *     {"message":"hello, world!"}
- */
-data class JsonMessage(val message: String)
