7392069df362875cbc716186a44f35206540411b
Chris Kent <cjkent@hotmail.com>
2019-05-18 22:08:46 +0100

    Comments on main function for local server

--- archetype/src/main/resources/archetype-resources/local-server/src/main/kotlin/localserver/Main.kt
+++ archetype/src/main/resources/archetype-resources/local-server/src/main/kotlin/localserver/Main.kt
@@ -8,6 +8,13 @@ import ${package}.core.api
 import ${package}.core.config
 import ${package}.core.createComponents
 
+/**
+ * Main function for running the application in an HTTP server (Jetty).
+ *
+ * The arguments are:
+ *   * `-p`, `--port` - the port the server listens on. Defaults to 8080.
+ *   * `-r`, `--root` - the context root. This is the base path from which everything is served.
+ */
 fun main(args: Array<String>) {
     val serverArgs = ServerArgs()
     JCommander.newBuilder().addObject(serverArgs).build().parse(*args)
