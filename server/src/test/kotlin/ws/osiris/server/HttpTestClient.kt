17375fb078f594911285cbe66ee3702fb337b7e3
cjkent <cjkent@hotmail.com>
2024-02-10 10:54:57 +0000

    Bump Kotlin version, fix after OkHttp upgrade

--- server/src/test/kotlin/ws/osiris/server/HttpTestClient.kt
+++ server/src/test/kotlin/ws/osiris/server/HttpTestClient.kt
@@ -1,9 +1,9 @@
 package ws.osiris.server
 
-import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.slf4j.LoggerFactory
 import ws.osiris.core.Headers
 import ws.osiris.core.HttpHeaders
@@ -36,18 +36,18 @@ class HttpTestClient(
         val request = Request.Builder().url(buildPath(path)).build()
         log.debug("Making request {}", request)
         val response = client.newCall(request).execute()
-        val testResponse = TestResponse(response.code(), response.headerMap(), response.body()?.string())
+        val testResponse = TestResponse(response.code, response.headerMap(), response.body?.string())
         log.debug("Received response {}", testResponse)
         return testResponse
     }
 
     override fun post(path: String, body: String, headers: Map<String, String>): TestResponse {
-        val mediaType = headers[HttpHeaders.CONTENT_TYPE]?.let { MediaType.parse(it) }
-        val requestBody = RequestBody.create(mediaType, body)
+        val mediaType = headers[HttpHeaders.CONTENT_TYPE]?.toMediaType()
+        val requestBody = body.toRequestBody(mediaType)
         val request = Request.Builder().url(buildPath(path)).post(requestBody).build()
         log.debug("Making request {}", request)
         val response = client.newCall(request).execute()
-        val testResponse = TestResponse(response.code(), response.headerMap(), response.body()?.string())
+        val testResponse = TestResponse(response.code, response.headerMap(), response.body?.string())
         log.debug("Received response {}", testResponse)
         return testResponse
     }
@@ -56,7 +56,7 @@ class HttpTestClient(
         val request = Request.Builder().url(buildPath(path)).method("OPTIONS", null).build()
         log.debug("Making request {}", request)
         val response = client.newCall(request).execute()
-        val testResponse = TestResponse(response.code(), response.headerMap(), null)
+        val testResponse = TestResponse(response.code, response.headerMap(), null)
         log.debug("Received response {}", testResponse)
         return testResponse
     }
@@ -64,7 +64,7 @@ class HttpTestClient(
     private fun buildPath(path: String): String = "${protocol.protocolName}://$server:$port$basePath$path"
 
     private fun OkHttpResponse.headerMap(): Headers =
-        Headers(this.headers().toMultimap().mapValues { (_, list) -> list[0] })
+        Headers(this.headers.toMultimap().mapValues { (_, list) -> list[0] })
 
     companion object {
         private val log = LoggerFactory.getLogger(HttpTestClient::class.java)
